d470503f56da7a0f5539695ade8b275c541a6d2b
Maxim Ermolaev <ermolmak@gmail.com>
2017-02-20 18:07:51 +0300

    Tested

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 class Singleton {
  public:
   static Singleton *getObject();
 
+  std::chrono::system_clock::time_point getTime();
+
  private:
-  Singleton() {}
+  Singleton() : time(std::chrono::system_clock::now()) {}
   Singleton(Singleton &) = delete;
 
   static Singleton *object;
+  std::chrono::system_clock::time_point time;
 };
 
 Singleton *Singleton::getObject() {
@@ -19,8 +24,21 @@ Singleton *Singleton::getObject() {
   return object;
 }
 
+std::chrono::system_clock::time_point Singleton::getTime() {
+  return time;
+}
+
 Singleton *Singleton::object = nullptr;
 
 int main() {
+  auto time1 = Singleton::getObject()->getTime();
+  std::this_thread::sleep_for(std::chrono::duration<int, std::milli>(3000));
+  auto time2 = Singleton::getObject()->getTime();
+
+  std::cout << std::chrono::duration<double, std::milli>(time2 - time1).count() << std::endl;
+
+//  Singleton singleton;
+
+//  Singleton singleton = *Singleton::getObject();
   return 0;
 }
